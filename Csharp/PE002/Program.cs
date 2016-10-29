855a3dacb85e70f2a396d7c7c4f31aee4b047115
Carlos Vega <veglos@Carloss-MacBook-Pro.local>
2016-10-29 02:39:25 -0300

    Initial commit

---
+++ Csharp/PE002/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//not 4194302
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int limit = 4000000;
+            int sum = 0;
+            int lastTerm = 0;
+            int currentTerm = 1;
+            int nextTerm = 0;
+            while (currentTerm < limit)
+            {
+                if (isEven(currentTerm))
+                {
+                    sum += currentTerm;
+                }
+
+                
+                nextTerm = lastTerm + currentTerm;
+                lastTerm = currentTerm;
+                currentTerm = nextTerm;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static bool isEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+    }
+}
