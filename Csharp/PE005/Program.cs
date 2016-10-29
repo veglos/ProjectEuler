d036cb030fe752f951b2a5ddc8e0b54e48bd44f0
Carlos Vega <cvegah01@gmail.com>
2016-10-29 19:45:44 -0300

    Initial commit

---
+++ Csharp/PE005/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long smallestNumber = 20;
+            long divisors = 0;
+            while(divisors != 20){
+                
+                for(long i=1;i<=20;i++){
+                    if(smallestNumber % i == 0){
+                        divisors++;
+                    }
+                }
+
+                if(divisors != 20){
+                    divisors = 0;
+                    smallestNumber++;
+                }
+            }
+
+            Console.WriteLine("Smallest positive number is {0}", smallestNumber);
+        }
+    }
+}
