ad8801c78bc3a24e7254376a7a0bf983ec594920
Carlos Vega <cvegah01@gmail.com>
2016-10-30 00:30:57 -0300

    Initial commit

---
+++ Csharp/PE009/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long finalSum = 1000;
+            long product = 0;
+            bool tripletFound = false;
+
+            long x,y,z;
+            x = y = z = 0;
+
+            for(long c=finalSum;c>0;c--){
+                if(tripletFound) break;
+                for(long b=c-1;b>0;b--){
+                    if(tripletFound) break;
+                    for(long a=b-1;a>0;a--){
+                        if(a+b+c == finalSum && a*a + b*b == c*c){
+                            tripletFound = true;
+                            product = a*b*c;
+                            x = a;
+                            y = b;
+                            z = c;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("The product of {0} x {1} x {2} is {3}", x,y,z,product);
+        }
+    }
+}
---
+++ Csharp/PE010/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long limit = 2000000;
+            long sum = 0;
+            for(long i=2;i<limit;i++){
+                if(isPrime(i)){
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("Find the sum of all the primes below {0} is {1}",limit,sum);
+        }
+
+        public static bool isPrime(long number){
+            if(number <= 1) return false;
+
+            int divisors = 0;
+            double sqroot = Math.Sqrt(number);
+            for(int i=2; i<=sqroot;i++){
+                if(number % i == 0){
+                    divisors++;
+                }
+            }
+            
+            return (divisors == 0);
+        }
+    }
+}
