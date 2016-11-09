75dcb10cbeab977cdda16643156f12208e47bf74
Carlos Vega <cvegah01@gmail.com>
2016-11-08 23:37:01 -0300

    Initial commit

---
+++ Csharp/PE019/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {  
+            
+        }
+
+
+        private static int GetDaysBetweenYears(int firstyear, int lastyear)
+        {
+            int days = 0;
+
+            for (int i = firstyear; i <= lastyear; i++)
+            {
+                days += (IsLeapYear(i)) ? 366 : 365;
+            }
+
+            return days;
+        }
+
+        private static bool IsLeapYear(int year)
+        {
+            return !(year % 4 == 0 && year != 2000);
+        }
+
+
+    }
+}
