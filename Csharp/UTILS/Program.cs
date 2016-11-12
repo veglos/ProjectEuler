91cb22983f8d42014be3f155d423c5d67c65d989
Carlos Vega <cvegah01@gmail.com>
2016-11-12 11:53:41 -0300

    Project for useful functions and classes

---
+++ Csharp/UTILS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BigNumbers bigNumbers = new BigNumbers();
+            string factorial = "1";
+            for(int i=1;i<=100;i++){
+                factorial = bigNumbers.Multiply(factorial,i.ToString());
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
