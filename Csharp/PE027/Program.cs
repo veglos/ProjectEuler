201e88d05d9888cd3f770ded8bf0359d2c1cbcaf
Carlos Vega <cvegah01@gmail.com>
2016-11-19 15:40:07 -0300

    Initial commit

---
+++ Csharp/PE027/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int maxPrimes = 0;
+            int coefA = 0;
+            int coefB = 0;
+
+            for (int b = 1000; b >= -1000; b--)
+            {
+                for (int a = 999; a >= -999; a--)
+                {
+                    int n = 0;
+                    int foundPrimes = 0;
+                    while (isPrime(n * n + n * a + b))
+                    {
+                        n++;
+                        foundPrimes++;
+                    }
+                    if (maxPrimes < foundPrimes)
+                    {
+                        maxPrimes = foundPrimes;
+                        coefA = a;
+                        coefB = b;
+                    }
+                }
+            }
+
+            Console.WriteLine("{0}x{1} = {2}", coefA, coefB, coefA * coefB);
+        }
+
+
+
+        public static bool isPrime(long number)
+        {
+            if (number <= 1) return false;
+
+            int divisors = 0;
+            double sqroot = Math.Sqrt(number);
+            for (int i = 2; i <= sqroot; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisors++;
+                }
+            }
+
+            return (divisors == 0);
+        }
+    }
+}
