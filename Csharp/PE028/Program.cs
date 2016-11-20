6ce030e8292834aa2daee0359ed4c184627974c8
Carlos Vega <cvegah01@gmail.com>
2016-11-20 13:18:14 -0300

    Initial commit

---
+++ Csharp/PE028/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int dimension = 1001;
+            int number = dimension * dimension;
+            int difference = dimension - 1;
+            int sum = 0;
+
+            while (number > 1)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    sum += number;                    
+                    number -= difference;
+                }
+                difference -= 2;
+            }
+            sum++;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
